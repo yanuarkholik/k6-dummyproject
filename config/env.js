e523e1daac7732423f932d2ca4e21ff0386b8758
yanuarkholik <yanuar.kholik@javan.co.id>
2026-01-05 14:07:47 +0700

    update env

--- config/env.js
+++ config/env.js
@@ -1,7 +1,13 @@
 export const config = {
   // base url
-  baseURL: __ENV.baseURL || 'dummyjson.com',
+  baseURL: __ENV.baseURL || 'https://dummyjson.com',
 
   username: __ENV.username || 'emilys',
   password: __ENV.password || 'emilyspass',
+
+  // system tags 
+  systemTags: {
+    environment: __ENV.ENV || 'staging',
+    project: 'galeri-24-k6',
+  },
 }
